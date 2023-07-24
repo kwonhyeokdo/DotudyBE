19a59bdf53c22ba46e167d8c392de91b4a79d98a
kwonhyeokdo <kwonhyeokdo1209@gmail.com>
2023-07-24 18:49:46 +0900

    spring boot init

---
+++ src/main/java/com/dotudy/dotudy_be/DotudyBeApplication.java
@@ -0,0 +1,13 @@
+package com.dotudy.dotudy_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotudyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotudyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dotudy/dotudy_be/DotudyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotudy.dotudy_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotudyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
